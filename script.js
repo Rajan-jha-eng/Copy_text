0f79ca3864741f1ac987ab7ee8fbccb70542bc9d
Rajan Jha <54759854+Rajan-jha-eng@users.noreply.github.com>
2022-07-13 22:11:18 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function selectText() {
-  var text = document.getElementById('txtBox1');
-  text.select();
-  text.setSelectionRange(0, 1000);
-  navigator.clipboard.writeText(text.value);
-}
-    
