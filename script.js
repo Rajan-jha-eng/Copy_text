079e5e145e3f7f71e9f5e96dc46891b62812323c
Rajan Jha <54759854+Rajan-jha-eng@users.noreply.github.com>
2022-07-13 22:03:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function selectText() {
-  var text = document.getElementById('textBox1');
+  var text = document.getElementById('txtBox1');
   text.select();
   text.setSelectionRange(0, 1000);
   navigator.clipboard.writeText(text.value);
